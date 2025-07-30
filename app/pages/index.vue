2ce7464f4e20fc93c7df1fa0578f503c6dd271d5
Yuri de Snaijer <yuri.desnaijer@incentro.com>
2025-07-30 09:38:59 +0200

    init

---
+++ app/pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useGeolocation } from '@vueuse/core'
+
+const { coords } = useGeolocation()
+
+</script>
+
+<template>
+  <UContainer>
+  <LMap
+      style="height: 350px"
+      :zoom="50"
+      :center="[coords.latitude, coords.longitude]"
+      :use-global-leaflet="false"
+  >
+    <LTileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+        layer-type="base"
+        name="OpenStreetMap"
+    />
+  </LMap>
+
+  <UButton>Save</UButton>
+  </UContainer>
+</template>

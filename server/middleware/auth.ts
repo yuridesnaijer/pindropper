81961fe50159234f9f7c544b6d88e94410a3a451
Yuri de Snaijer <yuri.desnaijer@incentro.com>
2025-07-31 21:43:27 +0200

    make server middleware fail softly

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,13 +1,9 @@
 import { serverSupabaseUser } from "#supabase/server";
-export default defineEventHandler(async (event) => {
-  const user = await serverSupabaseUser(event);
-  if (!user) {
-    throw createError({
-      statusCode: 401,
-      statusMessage: "Unauthorized",
-      message: "You must be logged in to access this resource.",
-    });
-  }
 
-  event.context.user = user;
+export default defineEventHandler(async (event) => {
+  try {
+    event.context.user = await serverSupabaseUser(event);
+  } catch (error) {
+    console.error("Auth error: ", error);
+  }
 });
